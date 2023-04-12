98106e4a37eeb12c005401bd54f19fc3395832bd
Marcin Siwonia <mahinek12@gmail.com>
2023-04-12 20:32:16 +0200

    Add hover and link feature to FeatureBoxes and fix for yarn.lock

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -5,8 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './FeatureBox.module.scss';
 
-const FeatureBox = ({ active, icon, children }) => (
-  <div className={styles.root + (active ? ' ' + styles.active : '')}>
+const FeatureBox = ({ active, icon, children, onMouseEnter, onMouseLeave }) => (
+  <div
+    className={styles.root + (active ? ' ' + styles.active : '')}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+  >
     {icon && (
       <div className={styles.iconWrapper}>
         <FontAwesomeIcon className={styles.icon} icon={icon} />
@@ -15,11 +19,12 @@ const FeatureBox = ({ active, icon, children }) => (
     <div className={styles.content}>{children}</div>
   </div>
 );
-
 FeatureBox.propTypes = {
   children: PropTypes.node,
   icon: PropTypes.object,
   active: PropTypes.bool,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
 };
 
 export default FeatureBox;
--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 import {
   faTruck,
@@ -11,38 +12,78 @@ import {
 import styles from './FeatureBoxes.module.scss';
 import FeatureBox from '../../common/FeatureBox/FeatureBox';
 
-const FeatureBoxes = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-          <FeatureBox icon={faTruck} active>
-            <h5>Free shipping</h5>
-            <p>All orders</p>
-          </FeatureBox>
-        </div>
-        <div className='col'>
-          <FeatureBox icon={faHeadphones}>
-            <h5>24/7 customer</h5>
-            <p>support</p>
-          </FeatureBox>
-        </div>
-        <div className='col'>
-          <FeatureBox icon={faReplyAll}>
-            <h5>Money back</h5>
-            <p>guarantee</p>
-          </FeatureBox>
-        </div>
-        <div className='col'>
-          <FeatureBox icon={faBullhorn}>
-            <h5>Member discount</h5>
-            <p>First order</p>
-          </FeatureBox>
+const FeatureBoxes = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleMouseEnter = index => {
+    setActiveIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveIndex(null);
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <Link to=''>
+              <FeatureBox
+                icon={faTruck}
+                active={activeIndex === 0}
+                onMouseEnter={() => handleMouseEnter(0)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <h5>Free shipping</h5>
+                <p>All orders</p>
+              </FeatureBox>
+            </Link>
+          </div>
+          <div className='col'>
+            <Link to=''>
+              <FeatureBox
+                icon={faHeadphones}
+                active={activeIndex === 1}
+                onMouseEnter={() => handleMouseEnter(1)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <h5>24/7 customer</h5>
+                <p>support</p>
+              </FeatureBox>
+            </Link>
+          </div>
+          <div className='col'>
+            <Link to=''>
+              <FeatureBox
+                icon={faReplyAll}
+                active={activeIndex === 2}
+                onMouseEnter={() => handleMouseEnter(2)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <h5>Money back</h5>
+                <p>guarantee</p>
+              </FeatureBox>
+            </Link>
+          </div>
+          <div className='col'>
+            <Link to=''>
+              <FeatureBox
+                icon={faBullhorn}
+                active={activeIndex === 3}
+                onMouseEnter={() => handleMouseEnter(3)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <h5>Member discount</h5>
+                <p>First order</p>
+              </FeatureBox>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 FeatureBoxes.propTypes = {
   children: PropTypes.node,
