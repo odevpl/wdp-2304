61f91970261dcffe6343c5415314d4694154038f
Jan Kędzierski <jkedzierski087@gmail.com>
2023-04-17 19:24:24 +0200

    add promotions section

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,10 +6,12 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import SectionBrands from '../../features/SectionBrands/SectionBrands';
+import SectionPromotions from '../../features/SectionPromotions/SectionPromotions';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
+    <SectionPromotions />
     <NewFurniture />
     <SectionBrands />
   </div>
