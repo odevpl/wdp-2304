6765151cc9497681d6fd64f1fb52516139f48f34
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-04-17 16:47:46 +0200

    clean code

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
 import Swipeable from '../Swipeable/Swipeable';
