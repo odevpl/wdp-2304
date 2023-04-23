fcf4605094558981a31c803004762bb5a8bc0d32
Pawel Domagala <domagalapawel11@gmail.com>
2023-04-24 00:23:24 +0200

    Add search subpage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
 import Blog from './components/views/Blog/Blog';
+import Search from './components/views/Search/Search';
 
 const App = () => (
   <Provider store={store}>
@@ -21,6 +22,7 @@ const App = () => (
           <Route exact path={'/shop/:categoryId'} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
           <Route exact path={'/blog'} component={Blog} />
+          <Route exact path={'/search'} component={Search} />
         </Switch>
       </MainLayout>
     </BrowserRouter>
