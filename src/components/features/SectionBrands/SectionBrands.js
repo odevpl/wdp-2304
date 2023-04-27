4b71844be110689a94fbba90cb35225f3775c6bd
Marcin Siwonia <mahinek12@gmail.com>
2023-04-27 18:24:33 +0200

    Add better number of slides in different resolutions

--- src/components/features/SectionBrands/SectionBrands.js
+++ src/components/features/SectionBrands/SectionBrands.js
@@ -2,33 +2,57 @@ import React from 'react';
 import styles from './SectionBrands.module.scss';
 import { getBrands } from '../../../redux/brandsRedux';
 import { useSelector } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import TinySlider from 'tiny-slider-react';
+import 'tiny-slider/dist/tiny-slider.css';
+import './SectionBrandsSlider.scss';
 
 const SectionBrands = () => {
   const brands = useSelector(state => getBrands(state));
 
+  const brandsArray = Object.values(brands);
+
+  const settings = {
+    lazyload: true,
+    mouseDrag: false,
+    nav: false,
+    container: '.cardContent',
+    controlsText: '<>',
+    gutter: 20,
+    responsive: {
+      0: {
+        items: 1,
+        slideBy: 1,
+      },
+      576: {
+        items: 2,
+        slideBy: 2,
+      },
+      768: {
+        items: 3,
+        slideBy: 3,
+      },
+      992: {
+        items: 4,
+        slideBy: 4,
+      },
+      1200: {
+        items: 6,
+        slideBy: 6,
+      },
+    },
+  };
+
   return (
     <div className={`container ${styles.root}`}>
       <div className={styles.card}>
-        <div className={styles.cardContent}>
-          <FontAwesomeIcon icon={faChevronLeft} className={styles.arrow} />
-          <div className={styles.brandImage}>
-            <img src={brands.firstBrand} alt='brand' />
-          </div>
-          <div className={styles.brandImage}>
-            <img src={brands.secondBrand} alt='brand' />
-          </div>
-          <div className={styles.brandImage}>
-            <img src={brands.thirdBrand} alt='brand' />
-          </div>
-          <div className={styles.brandImage}>
-            <img src={brands.fourthBrand} alt='brand' />
-          </div>
-          <div className={styles.brandImage}>
-            <img src={brands.fifthBrand} alt='brand' />
-          </div>
-          <FontAwesomeIcon icon={faChevronRight} className={styles.arrow} />
+        <div className={`cardContent ${styles.cardContent}`}>
+          <TinySlider settings={settings}>
+            {brandsArray.map(brand => (
+              <div className={styles.brandImage} key={brand}>
+                <img src={brand} alt='brand' />
+              </div>
+            ))}
+          </TinySlider>
         </div>
       </div>
     </div>
